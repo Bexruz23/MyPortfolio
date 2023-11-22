e475013dadb0980f45aec8072599c4bb292f9cac
Behruz <behruz@MacBook-Pro-Behruz.local>
2023-11-22 21:21:32 +0500

    Carousel is fixed

--- src/components/Kinolar/Carousel.jsx
+++ src/components/Kinolar/Carousel.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
-import "./carousel.css";
+import "./Carousel.css";
 
 export default function Carousel({ onClick, tab, data }) {
-  console.log(tab, "tab");
   return (
     <div className="container flex items-center mt-3">
       <h1 className="text-3xl pr-4">Trending</h1>
